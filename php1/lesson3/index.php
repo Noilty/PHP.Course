19d875bef2a6f0f8c853206e190a856736e7d339
Noilty <thenoilty@gmail.com>
2020-05-22 15:04:59 +0700

    fix: lesson3

--- php1/lesson3/index.php
+++ php1/lesson3/index.php
@@ -125,32 +125,69 @@ $arrMenu = [
     'Menu 3' => 'Link-Menu-3'
 ]
 ?>
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-    <ul>
-    <? foreach ($arrMenu as $titleMenuOneLv => $titleMenuTwoLv) { ?>
-        <li>
-            <?= (is_array($titleMenuTwoLv)) ? "$titleMenuOneLv" : "<a href='$titleMenuTwoLv'>$titleMenuOneLv</a>"; ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+<? foreach ($arrMenu as $titleMenuOneLv => $titleMenuTwoLv) { ?>
+    <li>
+        <?= (is_array($titleMenuTwoLv)) ? "$titleMenuOneLv" : "<a href='$titleMenuTwoLv'>$titleMenuOneLv</a>"; ?>
 
-            <? if (is_array($titleMenuTwoLv)) { ?>
-                <ul>
-                    <? foreach ($titleMenuTwoLv as $menuTitle => $menuLink) { ?>
-                        <li><a href="<?= $menuLink ?>"><?= $menuTitle ?></a></li>
-                    <? } ?>
-                </ul>
-            <? } ?>
-        </li>
-    <? } ?>
-    </ul>
-    </body>
-    </html>
+        <? if (is_array($titleMenuTwoLv)) { ?>
+            <ul>
+                <? foreach ($titleMenuTwoLv as $menuTitle => $menuLink) { ?>
+                    <li><a href="<?= $menuLink ?>"><?= $menuTitle ?></a></li>
+                <? } ?>
+            </ul>
+        <? } ?>
+    </li>
+<? } ?>
+</ul>
+</body>
+</html>
 <?php
 // 6 <<<
+echo '<hr />';
+// 7 >>>
+for ($i = 0; $i < 10; printf('%s ', $i++)) {
+    // здесь пусто
+}
+// 7 <<<
+echo '<hr />';
+// 8 >>>
+$arrRegions = [
+    'Московская область' => [
+        'Москва',
+        'Зеленоград',
+        'Клин'
+    ],
+    'Ленинградская область' => [
+        'Санкт-Петербург',
+        'Всеволожск',
+        'Павловск',
+        'Кронштадт'
+    ],
+    'Рязанская область' => [
+        'Касимов',
+        'Рыбное',
+        'Скопин',
+        'Шацк'
+    ]
+];
+foreach ($arrRegions as $region => $cities) {
+    echo "<b>{$region}</b>:<br />";
+    foreach ($cities as $city) {
+        if (mb_substr($city,0,1,'UTF-8')  === 'К') {
+            echo "{$city}, ";
+        }
+    }
+    echo "<br />";
+}
+// 8 <<<
