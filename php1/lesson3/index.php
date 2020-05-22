c1c1c14c70f7a6c2811a1532ddfb499c866ed57c
Noilty <thenoilty@gmail.com>
2020-05-22 14:29:39 +0700

    fix: lesson3

--- php1/lesson3/index.php
+++ php1/lesson3/index.php
@@ -69,7 +69,7 @@ $alph = [
     '!', ',', ' '
 ];
 
-var_dump(transliteration('привет, мир!', $alph));
+echo transliteration('привет, мир!', $alph);
 
 /**
  * Функция транслитерации строк
@@ -78,8 +78,79 @@ var_dump(transliteration('привет, мир!', $alph));
  * @return string
  */
 function transliteration($text, $alph) {
-    $string = str_split($text, 2);
-    var_dump($string);
+//    $string = str_split($text, 2);
+    $string = mb_str_split($text);
+    foreach ($string as &$ru_symbol) {
+        foreach ($alph as $ru => $en) {
+            if ($ru_symbol === $ru) {
+                $ru_symbol = $en;
+            }
+        }
+    }
+    return implode($string);
+}
 
+/**
+ * str_split кириллица
+ * @param $str
+ * @return mixed
+ */
+function mb_str_split($str)
+{
+    preg_match_all('#.{1}#uis', $str, $out);
+    return $out[0];
 }
 // 4 <<<
+echo '<hr />';
+// 5 >>>
+echo spaceForUnderscores('Рома Кузьмин');
+
+/**
+ * Функция заменяет в строке пробелы на подчеркивания
+ */
+function spaceForUnderscores($str) {
+    return str_replace(' ', '_', $str);
+}
+// 5 <<<
+echo '<hr />';
+// 6 >>>
+$arrMenu = [
+    'Menu 1' => [
+        'FAQ' => 'Link-FAQ',
+        'Helper' => 'Link-Helper',
+        'About' => 'Link-About',
+        'Contact' => 'Link-Contact'
+    ],
+    'Menu 2' => 'Link-Menu-2',
+    'Menu 3' => 'Link-Menu-3'
+]
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <ul>
+    <? foreach ($arrMenu as $titleMenuOneLv => $titleMenuTwoLv) { ?>
+        <li>
+            <?= (is_array($titleMenuTwoLv)) ? "$titleMenuOneLv" : "<a href='$titleMenuTwoLv'>$titleMenuOneLv</a>"; ?>
+
+            <? if (is_array($titleMenuTwoLv)) { ?>
+                <ul>
+                    <? foreach ($titleMenuTwoLv as $menuTitle => $menuLink) { ?>
+                        <li><a href="<?= $menuLink ?>"><?= $menuTitle ?></a></li>
+                    <? } ?>
+                </ul>
+            <? } ?>
+        </li>
+    <? } ?>
+    </ul>
+    </body>
+    </html>
+<?php
+// 6 <<<
