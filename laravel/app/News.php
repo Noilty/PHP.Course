b7748b0a0b3efdf16db03701d72282ec14777eac
Noilty <thenoilty@gmail.com>
2020-08-30 13:59:03 +0700

    fix

--- laravel/app/News.php
+++ laravel/app/News.php
@@ -4,63 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class News
+ * @package App
+ *
+ * @property string title
+ * @property string text
+ * @property string image
+ * @property boolean isPrivate
+ */
 class News extends Model
 {
-    public static $news = [
-        1 => [
-            'id' => 1,
-            'title' => 'News 1',
-            'text' => 'News text 1',
-            'category_id' => '1',
-            'image' => null,
-            'isPrivate' => false
-        ],
-        2 => [
-            'id' => 2,
-            'title' => 'News 2',
-            'text' => 'News text 2',
-            'category_id' => '2',
-            'image' => null,
-            'isPrivate' => false
-        ],
-        3 => [
-            'id' => 3,
-            'title' => 'News 3',
-            'text' => 'News text 3',
-            'category_id' => '1',
-            'image' => null,
-            'isPrivate' => false
-        ],
-        4 => [
-            'id' => 4,
-            'title' => 'News 4',
-            'text' => 'News text 4',
-            'category_id' => '1',
-            'image' => null,
-            'isPrivate' => false
-        ]
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'text',
+        'isPrivate'
     ];
-
-    public static $category = [
-        1 => [
-            'id' => 1,
-            'title' => 'Спорт',
-            'title_translit' => 'sport'
-        ],
-        2 => [
-            'id' => 2,
-            'title' => 'Политика',
-            'title_translit' => 'politika'
-        ],
-    ];
-
-    public static function getNews():array
-    {
-        return json_decode(\Storage::disk('local')->get('news.json'), true);
-    }
-
-    public static function getCategories():array
-    {
-        return json_decode(\Storage::disk('local')->get('category.json'), true);
-    }
 }
